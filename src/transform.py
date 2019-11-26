e7c31eeb5bbef70dcd58a674c2e0944b17ad17c8
Aaron Campbell <sharkbeard.io@gmail.com>
2019-11-26 06:24:02 -0500

    fix error in joins

--- src/transform.py
+++ src/transform.py
@@ -4,20 +4,32 @@ connection = sqlite3.connect('../lib/cma-artworks.db')
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
-cursor.execute("SELECT artwork.*, department.name AS department, "
-               "creator.role AS creator_role, creator.description AS creator_description "
+cursor.execute("SELECT artwork.*, department.name AS department_name, "
+               "GROUP_CONCAT(DISTINCT artwork__creator.creator_id) AS creator_ids "
                "FROM artwork "
                "LEFT JOIN artwork__creator "
                "ON artwork.id = artwork__creator.artwork_id "
-               "LEFT JOIN creator "
-               "ON artwork__creator.creator_id = creator.id "
                "LEFT JOIN artwork__department "
                "ON artwork.id = artwork__department.artwork_id "
                "LEFT JOIN department "
-               "ON artwork__department.department_id = department.id"
+               "ON artwork__department.department_id = department.id "
+               "GROUP BY artwork.id"
                )
 results = cursor.fetchall()
+artworks = [dict(row) for row in results]
 
-results = [dict(row) for row in results]
+cursor.execute("SELECT * FROM creator")
+results = cursor.fetchall()
+creators = [dict(row) for row in results]
+
+for artwork in artworks:
+    if artwork['creator_ids'] is not (None):
+        artwork_creator_ids = artwork['creator_ids'].split(',')
+        artwork['creators'] = []
+        for creator in creators:
+            cid = creator['id']
+            if cid in artwork_creator_ids:
+                artwork['creators'].append({'name': creator['description'], 'role': creator['role']})
+
 with open('../dist/artwork.json', 'w') as output:
-    json.dump(results, output, sort_keys=True, indent=4)
+    json.dump(artworks, output, sort_keys=True, indent=4)
