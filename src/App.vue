8d679d7350a5f376fc04c0a2f0006e0689941b90
Aaron Campbell <sharkbeard.io@gmail.com>
2019-11-26 06:34:48 -0500

    add vue files

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <el-row :gutter="0">
+      <el-col v-for="(artwork, index) in artworks" :key="index" :span="4">
+        <Artwork :data="artwork"/>
+      </el-col>
+    </el-row>
+    <el-button @click="console.log(artworks);">Test</el-button>
+  </div>
+</template>
+
+<script>
+import Artwork from './components/Artwork.vue';
+// const axios = require('axios').default;
+// export function readFile(file) {
+//   return new Promise((resolve) => {
+//     let reader = new FileReader();
+//     reader.onLoad = () => resolve(reader.result);
+//     reader.readAsText(file);
+//   });
+// }
+// export async function getInput(instance) {
+//   instance.artworks = await readFile('./artwork.json');
+// }
+let artworks = [];
+import(
+        /* webpackChunkName: "jsonArtwork" */
+        '../public/artwork.json')
+        .then(({default: jsonArtwork}) => jsonArtwork);
+        // .then(({default: jsonArtwork}) => {
+        //     artworks = jsonArtwork;
+        // });
+import jsonData from '../public/artwork.json';
+artworks = jsonData;
+export default {
+  // name: 'app',
+  data: function() {
+    return { artworks: [] }
+  },
+  created: function()
+  {
+    this.artworks = [{
+      "accession_number": "2015.584",
+      "creator_description": "John Taylor Arms (American, 1887-1953)",
+      "creator_role": "artist",
+      "department": "Prints",
+      "id": "86508",
+      "title": "Gates of the City",
+      "tombstone": "New York Series: Gates of the City, 1922. John Taylor Arms (American, 1887-1953). Color etching and aquatint; platemark: 21.5 x 20.2 cm (8 7/16 x 7 15/16 in.); sheet: 31.1 x 26.2 cm (12 1/4 x 10 5/16 in.). The Cleveland Museum of Art, Gift of Carole W. and Charles B. Rosenblatt 2015.584 Courtesy of John Taylor Arms/ \u00a9 Suzanne Arms Hawkins"
+    }];
+    this.artworks = artworks;
+    // this.artworks = jsonArtwork;
+    // this.artworks = Array(9).fill(this.artworks[0])
+    // let instance = this;
+    // axios.get('/artwork.json')
+    //   .then(function(response) {
+    //     instance.artworks = response.body;
+    //   });
+    // getInput(this);
+  },
+  components: {
+    Artwork,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Artwork.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="artwork">
+    <el-card body-style="padding: 0; height: 150px">
+      <el-image :lazy="true" :src="img_path" style="min-width:100%; max-height:100px" class="image" fit="cover" />
+      <div class="card-details small" @click="opened = true">
+          {{data.title}}
+      </div>
+    </el-card>
+    <el-drawer :title="data.title" :visible.sync="opened" direction="btt">
+      <li><i class="el-icon-collection-tag"></i> {{ data.accession_number }}</li>
+      <li><i class="el-icon-brush"></i> {{ data.department }}</li>
+      <li class="small"><i class=""></i> {{ data.tombstone }}</li>
+      <el-divider>
+        <span v-if="data.creator_role != ''" class="creator-role"> {{ data.creator_role }}: </span>
+      </el-divider>
+      <li class="small">
+        <i class="el-icon-user"></i>
+        {{ data.creator_description }}
+      </li>
+    </el-drawer>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Artwork',
+  props: {
+    data: Object
+  },
+  data: function() { return {
+    opened: false
+  }},
+  computed: {
+    img_path: function() { return '/images/' + this.data.accession_number + '_reduced.jpg' }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.card-details {
+  padding: 20px;
+}
+.creator-role {
+  text-transform: capitalize;
+}
+.small {
+  font-size: 80%;
+}
+</style>
